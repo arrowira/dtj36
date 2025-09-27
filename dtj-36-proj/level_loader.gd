ae886b3e913c58bd9d66c99ff36eedc914021b11
drewp@bigasterisk.com <drewp@bigasterisk.com>
2025-09-27 16:53:29 -0700

    wip entrance fiding

--- dtj-36-proj/level_loader.gd
+++ dtj-36-proj/level_loader.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 func _ready() -> void:
 	var num = nextLevel.level
-	var levObjs = load("res://levels/level%d.tscn" % num).instantiate()
+	var levObjs : Node = load("res://levels/level%d.tscn" % num).instantiate()
 	add_child(levObjs)
 	var hud: Label = get_node("/root/main/hud/levelNumber")
 	hud.text = "Level %d" % num
+
+	#var ent = levObjs.find_child('Entrance')
+	#print(ent, ent.position)
+	
