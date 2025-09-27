2102ddea38f919da220a9f1fc97ad14911e3571b
drewp@bigasterisk.com <drewp@bigasterisk.com>
2025-09-27 14:10:51 -0700

    moues smoothing

--- dtj-36-proj/spot_light.gd
+++ dtj-36-proj/spot_light.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
+@export var mouseSpeed: float = 3
+
 
 func _physics_process(delta: float) -> void:
-	look_at(get_viewport().get_mouse_position())
-	if position.x > get_viewport().get_mouse_position().x+5 or position.x < get_viewport().get_mouse_position().x-5:
-		if position.y > get_viewport().get_mouse_position().y+5 or position.y < get_viewport().get_mouse_position().y-5:
-			transform.origin.x+=1
-		
+	var mp = get_viewport().get_mouse_position()
+	position = position.lerp(mp, delta * mouseSpeed) 
+	
