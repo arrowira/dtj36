f8c60eb79cfdf588888a10ac2e1590df3a8cf0aa
drewp@bigasterisk.com <drewp@bigasterisk.com>
2025-09-27 17:54:00 -0700

    entrance/exit platforms and sensing

--- dtj-36-proj/level-components/exit.gd
+++ dtj-36-proj/level-components/exit.gd
@@ -2,5 +2,4 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("hit exit " , body)
 	nextLevel.finishedLevel()
--- dtj-36-proj/next_level.gd
+++ dtj-36-proj/next_level.gd
@@ -4,6 +4,6 @@ var level = 1
 
 func finishedLevel():
 	nextLevel.level += 1
-	print('next level')
+	
 	get_tree().change_scene_to_file("res://congrats.tscn")
 	
