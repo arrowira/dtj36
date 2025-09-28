f4f2a3eba35860e9be40da5147f8357d169a613b
drewp@bigasterisk.com <drewp@bigasterisk.com>
2025-09-27 20:38:26 -0700

    don't move the light layer; send mouse pos into the shader

--- dtj-36-proj/spot_light.gd
+++ dtj-36-proj/spot_light.gd
@@ -5,5 +5,11 @@ extends Node2D
 
 func _physics_process(delta: float) -> void:
 	var mp = get_viewport().get_mouse_position()
-	position = position.lerp(mp, delta * mouseSpeed) 
+	var mouse = get_node("mousePos")
+	if mouse:
+		mouse.position = mouse.position.lerp(mp, delta * mouseSpeed) 
 	
+	var viewport_size = get_viewport_rect().size
+	var mouse_pos_normalized = mp / viewport_size
+	var ll = get_node('lightLayer')
+	ll.get_material().set_shader_parameter("mouse_position", mouse_pos_normalized)
