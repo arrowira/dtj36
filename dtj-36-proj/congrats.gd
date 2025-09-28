a6b9c79b6fd94530b908ad9711047d1570425065
drewp@bigasterisk.com <drewp@bigasterisk.com>
2025-09-27 23:25:50 -0700

    clean up congrats screen

--- dtj-36-proj/congrats.gd
+++ dtj-36-proj/congrats.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready() -> void:
+	get_node('msg').text = "Level %d passed" % (nextLevel.level - 1)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn") # Replace with function body.
