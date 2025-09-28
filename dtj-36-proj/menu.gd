7f9473f58013a5e075582c7c86585ae8e2170de7
drewp@bigasterisk.com <drewp@bigasterisk.com>
2025-09-27 17:32:43 -0700

    menu layout

--- dtj-36-proj/menu.gd
+++ dtj-36-proj/menu.gd
@@ -1,5 +1,5 @@
-extends Node3D
+extends Node2D
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://main.tscn")
