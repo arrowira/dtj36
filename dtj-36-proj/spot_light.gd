91e1dcc6057e4affccd04717a49d0fdf2fdf0224
arrowira <41136800+arrowira@users.noreply.github.com>
2025-09-27 23:42:56 -0700

    fixed

--- dtj-36-proj/spot_light.gd
+++ dtj-36-proj/spot_light.gd
@@ -11,10 +11,10 @@ func _ready() -> void:
 	var fudge = 1.02 # get the collision area to align with the spot
 	mousePosNode.scale = Vector2(r, r) * 250 * fudge
 
-	var obj = get_node('/root/main/levelLoader').get_children()[0].get_node('Entrance')
-	print('obj ', obj)
-	if obj:
-		print('found entrace')
+	#var obj = get_node('/root/main/levelLoader').get_children()[0].get_node('Entrance')
+	#print('obj ', obj)
+	#if obj:
+		#print('found entrace')
 		
 
 
