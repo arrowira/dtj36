96152c4922a7c0e83c9cab13468e9ac3341ebc71
arrowira <41136800+arrowira@users.noreply.github.com>
2025-09-27 20:45:47 -0700

    level 3

--- dtj-36-proj/level-components/platform.gd
+++ dtj-36-proj/level-components/platform.gd
@@ -12,16 +12,18 @@ func _ready() -> void:
 	$Collision.set_deferred("disabled", !defaultState)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	$AudioStreamPlayer.play()
-	$Sprite2D.frame=poppedFrame
-	$Collision.set_deferred("disabled", false)
+	if area.get_parent().name == "spotLight":
+		$AudioStreamPlayer.play()
+		$Sprite2D.frame=poppedFrame
+		$Collision.set_deferred("disabled", false)
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
-	print("exited")
-	$Collision.set_deferred("disabled", !defaultState)
-	if defaultState == true:
-		$Sprite2D.frame=poppedFrame
-	else:
-		$Sprite2D.frame=insetFrame
-	
+	if area.get_parent().name == "spotLight":
+		print("exited")
+		$Collision.set_deferred("disabled", !defaultState)
+		if defaultState == true:
+			$Sprite2D.frame=poppedFrame
+		else:
+			$Sprite2D.frame=insetFrame
+		
---
+++ dtj-36-proj/spinner.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _physics_process(delta: float) -> void:
+	rotation-=1/20.0
