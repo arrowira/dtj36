0227420e04993289b2cefb1b3a83dc10d7985ca7
arrowira <41136800+arrowira@users.noreply.github.com>
2025-09-27 18:33:22 -0700

    different block sizes

--- dtj-36-proj/level-components/platform.gd
+++ dtj-36-proj/level-components/platform.gd
@@ -1,18 +1,27 @@
 extends StaticBody2D
 
+@export var insetFrame = 1
+@export var poppedFrame = 0
 
 @export var defaultState = false
 func _ready() -> void:
-	$Sprite2D.frame=1
+	if defaultState == true:
+		$Sprite2D.frame=poppedFrame
+	else:
+		$Sprite2D.frame=insetFrame
 	$Collision.set_deferred("disabled", !defaultState)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	$AudioStreamPlayer.play()
-	$Sprite2D.frame=0
+	$Sprite2D.frame=poppedFrame
 	$Collision.set_deferred("disabled", false)
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	print("exited")
 	$Collision.set_deferred("disabled", !defaultState)
-	$Sprite2D.frame=1
+	if defaultState == true:
+		$Sprite2D.frame=poppedFrame
+	else:
+		$Sprite2D.frame=insetFrame
+	
